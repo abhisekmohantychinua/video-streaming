5eaff75fadc1f279304b66847b4cddffcc19adae
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-12-17 23:32:51 +0530

    bakcend

---
+++ streaming-server/src/main/java/dev/abhisek/streamingserver/StreamingServerApplication.java
@@ -0,0 +1,48 @@
+package dev.abhisek.streamingserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class StreamingServerApplication implements CommandLineRunner {
+
+    private final VideoService videoService;
+
+    @Autowired
+    public StreamingServerApplication(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingServerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        videoService.getAllVideoTitle().forEach(System.out::println);
+    }
+
+    @GetMapping(value = "/{title}", produces = "video/mp4")
+    public Mono<Resource> getVideoByTitle(@PathVariable String title) throws MalformedURLException {
+        return videoService.getVideoByTitle(title);
+    }
+
+    @GetMapping(value = "/")
+    public Mono<List<String>> getAllVideo() throws IOException {
+        return Mono.just(videoService.getAllVideoTitle());
+    }
+}
---
+++ streaming-server/src/main/java/dev/abhisek/streamingserver/VideoService.java
@@ -0,0 +1,32 @@
+package dev.abhisek.streamingserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class VideoService {
+    private static final String PATH_TO_VIDEOS = "./videos/";
+
+    public Mono<Resource> getVideoByTitle(String title) throws MalformedURLException {
+            Path pdfPath = Paths.get(PATH_TO_VIDEOS).resolve(title+".mp4");
+            Resource resource = new UrlResource(pdfPath.toUri());
+        return Mono.just(resource);
+    }
+
+    public List<String> getAllVideoTitle() throws IOException {
+        File folder= new File(PATH_TO_VIDEOS);
+        return Arrays.stream(folder.list()).toList();
+    }
+}
